8c6c3ad476a7463b3b2362c65b514e4eb8d2cdf5
Catriona Bruce <catriona.bruce@tum.de>
2023-11-19 02:20:43 +0100

    make variants a char array instead of the string array

--- hasher.cpp
+++ hasher.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>  // Added for std::setw and std::setfill
 #include <cstring>
 #include <openssl/md5.h>
-#include <thread>
 #include <vector>
-#include <iomanip>
 
 bool debug_mode = false; // Global flag for debugging
 
@@ -27,35 +26,46 @@ std::string calculate_md5(const char* data, size_t size) {
 }
 
 // Function to generate variants by flipping one bit at a time
-std::vector<std::string> generate_variants(const std::string& input_data) {
+std::vector<std::string> generate_variants(const char* input_data, size_t data_size) {
     std::vector<std::string> variants;
-    for (size_t i = 0; i < input_data.size() * 8; ++i) {
+    variants.reserve(data_size * 8);
+
+    for (size_t i = 0; i < data_size * 8; ++i) {
         size_t index = i / 8;
         size_t bit_position = i % 8;
-        std::string variant = input_data;
-        variant[index] ^= (1 << bit_position);
-        variants.push_back(variant);
+
+        char variant = input_data[index];
+        variant ^= (1 << bit_position);
+
+        // Append the modified byte to the variants
+        variants.push_back(std::string(input_data, data_size));
+        variants.back()[index] = variant;
 
         // Debugging: Print the variant in hex format
         if (debug_mode) {
             std::cout << "Variant " << i << ": ";
-            for (char c : variant) {
-                std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)(unsigned char)c;
+            for (size_t j = 0; j < data_size; ++j) {
+                std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)(unsigned char)variants.back()[j];
             }
             std::cout << "\n";
         }
     }
+
     return variants;
 }
 
-// Function to process a range of variants
-void process_variants(const std::vector<std::string>& variants, size_t start, size_t end, const std::string& target_hash) {
-    for (size_t i = start; i < end; ++i) {
-        std::string hashed_value = calculate_md5(variants[i].c_str(), variants[i].size());
+// Function to process variants and find collisions
+void process_variants(const std::vector<std::string>& variants, const char* target_hash, size_t data_size) {
+    for (size_t i = 0; i < variants.size(); ++i) {
+        std::string hashed_value = calculate_md5(variants[i].c_str(), data_size);
         if (hashed_value == target_hash) {
+            size_t changed_byte_position = i / 8;
+            char original_value = variants[i][changed_byte_position];
+
             std::cout << "Collision found! Original hash: " << target_hash << "\n";
-            std::cout << "Position of changed byte: " << i / 8 << "\n";
-            std::cout << "Hex value changed to: " << std::hex << static_cast<int>(variants[i][i % 8]) << "\n";
+            std::cout << "Position of changed byte: " << changed_byte_position << "\n";
+            std::cout << "Hex value changed to: " << std::hex << static_cast<int>(variants[i][changed_byte_position]) << "\n";
+            std::cout << "Original value of changed byte: " << std::hex << static_cast<int>(original_value) << "\n";
             std::exit(0);  // Exit the entire program when a collision is found
         }
     }
@@ -68,51 +78,27 @@ int main(int argc, char* argv[]) {
     }
 
     const char* file_path = argv[1];
-    const std::string target_hash = argv[2];
-    
+    const char* target_hash = argv[2];
+
     // Set debug mode flag if "-d" is provided
     if (argc == 4 && std::string(argv[3]) == "-d") {
         debug_mode = true;
     }
 
-    std::ifstream file(file_path, std::ios::binary | std::ios::ate);
+    std::ifstream file(file_path, std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << file_path << "\n";
         return 1;
     }
 
-    std::streamsize file_size = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    std::vector<char> file_data(file_size);
-    if (!file.read(file_data.data(), file_size)) {
-        std::cerr << "Error reading file: " << file_path << "\n";
-        return 1;
-    }
+    // Read the entire file into memory
+    std::vector<char> file_data((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
     // Generate variants
-    auto variants = generate_variants(file_data.data());
+    auto variants = generate_variants(file_data.data(), file_data.size());
 
-    // Number of threads to use
-    const size_t num_threads = std::thread::hardware_concurrency();
-    
-    // Calculate variants per thread
-    const size_t variants_per_thread = variants.size() / num_threads;
-
-    // Vector to store thread objects
-    std::vector<std::thread> threads;
-
-    // Launch threads
-    for (size_t i = 0; i < num_threads; ++i) {
-        size_t start = i * variants_per_thread;
-        size_t end = (i == num_threads - 1) ? variants.size() : (i + 1) * variants_per_thread;
-        threads.emplace_back(process_variants, std::ref(variants), start, end, std::cref(target_hash));
-    }
-
-    // Join threads
-    for (auto& thread : threads) {
-        thread.join();
-    }
+    // Process variants and find collisions
+    process_variants(variants, target_hash, file_data.size());
 
     std::cout << "No collision found.\n";
     return 0;
